8936a83f5b701c0ad5265a795f96f79d1cf2a8ff
Rafael Guilherme Bertholdo <rafaelgb@ciandt.com>
2015-05-22 14:01:27 -0300

    task de build

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,13 @@ var ts = require('gulp-typescript');
 var tsProject = ts.createProject('tsconfig.json');
 
 
-gulp.task('scripts', function() {
+gulp.task('build', function() {
     var tsResult = gulp.src('src/**/*.ts') // instead of gulp.src(...)
         .pipe(ts(tsProject));
 
     return tsResult.js.pipe(gulp.dest('built/local'));
 });
 
-gulp.task('watch', ['scripts'], function() {
+gulp.task('watch', ['build'], function() {
     gulp.watch('src/**/*.ts', ['scripts']);
 });
