0e851b032ac8ed3a5f6e806a57e3ad9aa5d4e1ae
Rafael Guilherme Bertholdo <rafaelgb@ciandt.com>
2015-05-22 14:24:28 -0300

    aff

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,13 @@ var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var tsProject = ts.createProject('tsconfig.json');
 
-
 gulp.task('build', function() {
-    var tsResult = gulp.src('src/**/*.ts') // instead of gulp.src(...)
+    var tsResult = gulp.src('src/**/*.ts') 
         .pipe(ts(tsProject));
 
     return tsResult.js.pipe(gulp.dest('built/local'));
 });
 
 gulp.task('watch', ['build'], function() {
-    gulp.watch('src/**/*.ts', ['scripts']);
+    gulp.watch('src/**/*.ts', ['build']);
 });
